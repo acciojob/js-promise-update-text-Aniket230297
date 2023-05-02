253827cd03cd9847997ebce8211274bab2b3ab31
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-05-02 09:36:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const promise1 = Promise.resolve("Hello, world!");
+const ele=document.getElementById("output");
+promise1.then((value) => {
+  
+ ele.innerHTML= value;
+}, 1000);
+
