60eac07d3754615a8cc3982dcb403b243c9f3af4
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-05-27 15:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(["Hello world!"]);
-        }, 1000);
-    });
+// const promise = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(["Hello world!"]);
+//         }, 1000);
+//     });
       
-    promise.then(values => {
-    //console.log(values)
-	document.getElementById("output").innerText = values;
-    });
+//     promise.then(values => {
+//     //console.log(values)
+// 	document.getElementById("output").innerText = values;
+//     });
+
+function updateOutput() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+updateOutput().then((message) => {
+  document.getElementById("output").innerText = message;
+});
+
